90869c8db0e34d46c3bdc02613f2dd097565a482
seleb <sean.s.leblanc@gmail.com>
2016-08-29 18:46:19 -0400

    boot sequence rounded to percentage

--- client/index.js
+++ client/index.js
@@ -233,7 +233,7 @@ function loadProgressHandler(loader, resource){
 	console.log("progress: " + loader.progress+"%");
 
 
-	$("#canvas-overlay pre").append("\n\t\t\t"+loader.progress+"...");
+	$("#canvas-overlay pre").append("\n\t\t\t"+Math.round(loader.progress)+"%...");
 }
 
 var game = new PIXI.Container();
